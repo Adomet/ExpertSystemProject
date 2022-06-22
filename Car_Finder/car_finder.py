f85135f12361137cc084240a22a4507a406305a8
Adomet <ahmedtalhabudak@gmail.com>
2022-06-22 09:26:50 +0300

    rename

--- Car_Finder/car_finder.py
+++ Car_Finder/car_finder.py
@@ -6,16 +6,16 @@ from pyke import krb_traceback
 
 engine = knowledge_engine.engine(__file__)
 
-def bc_test():
+def test():
 
     engine.reset()      # Allows us to run tests multiple times.
 
-    engine.activate('bc_simple_rules') #STUDENTS: you will need to edit the name of your rule file here
+    engine.activate('rules') #STUDENTS: you will need to edit the name of your rule file here
 
     print("doing proof")
     
     try:
-        with engine.prove_goal('bc_simple_rules.what_to_bring($bring)') as gen: #STUDENTS: you will need to edit this line
+        with engine.prove_goal('rules.what_to_bring($bring)') as gen: #STUDENTS: you will need to edit this line
             for vars, plan in gen:
                 print("You should bring: %s" % (vars['bring'])) #STUDENTS: you will need to edit this line
     except Exception:
@@ -29,16 +29,16 @@ def bc_test():
     #engine.print_stats()
 
     
-def bc_test_questions():
+def test_questions():
 
     engine.reset()      # Allows us to run tests multiple times.
 
-    engine.activate('bc_simple_rules_questions') #STUDENTS: you will need to edit the name of your rule file here
+    engine.activate('rules_questions') #STUDENTS: you will need to edit the name of your rule file here
 
     print("doing proof")
     
     try:
-        with engine.prove_goal('bc_simple_rules_questions.what_to_bring($bring)') as gen: #STUDENTS: you will need to edit this line
+        with engine.prove_goal('rules_questions.what_to_bring($bring)') as gen: #STUDENTS: you will need to edit this line
             for vars, plan in gen:
                 print("You should bring: %s" % (vars['bring'])) #STUDENTS: you will need to edit this line
 
